a10ff18e5da518149650561daa1cba05f6267720
abdullahsiddiqui9999 <abdullah.siddiqui9999@gmail.com>
2021-05-02 20:54:02 +0500

    Single page app 1.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,20 +4,33 @@ import DishDetail from "./DishDetailComponent";
 import { DISHES } from "../shared/dishes";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Contact from "./ContactComponent";
 import Home from "./HomeComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
 
     return (
@@ -26,6 +39,7 @@ class Main extends Component {
 
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route exact path="/contactus" component={Contact} />
           <Route
             exact
             path="/menu"
